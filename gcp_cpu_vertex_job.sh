dc61cc11aa19b0c05e865e26d562ec8caefca851
martinaegidius <gygridd@gmail.com>
2025-01-15 15:54:25 +0100

    updated gitignore

--- gcp_cpu_vertex_job.sh
+++ gcp_cpu_vertex_job.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # Load environment variables from .env
-set -a
-source .env
-set +a
+#effort for loading wandb from env -> seems impossible
+#set -a
+#source .env
+#set +a
 
 gcloud ai custom-jobs create \
     --region=europe-west1 \
     --display-name=test-a-train-run \
     --config=gcloud_vertex/config_cpu.yaml \
-    --env-vars WANDB_API_KEY=$WANDB_API_KEY,WANDB_ENTITY=$WANDB_ENTITY,WANDB_PROJECT=$WANDB_PROJECT
+    #--args WANDB_API_KEY=$WANDB_API_KEY,WANDB_ENTITY=$WANDB_ENTITY,WANDB_PROJECT=$WANDB_PROJECT
     # these are the arguments that are passed to the container, only needed if you want to change defaults
     #--command 'python src/my_project/train.py' \
     #--args '["--epochs", "10"]'
